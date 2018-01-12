c535f71c482b4b1255b177c32c93efabd27d79a8
Yaroslav Dynnikov <yaroslav.dynnikov@gmail.com>
2018-01-12 12:09:35 +0300

    Cleanup build

--- test/start.lua
+++ test/start.lua
@@ -1,14 +1,17 @@
 #!/usr/bin/env tarantool
 
+require('strict').on()
 local log = require('log')
 local fiber = require('fiber')
-require('strict').on()
 local watchdog = require('watchdog')
 
+local t0 = fiber.clock()
 watchdog.start(1)
-log.warn("fiber sleep ...")
 fiber.sleep(2)
-watchdog.stop()
+local t1 = fiber.clock()
+log.info("Still alive after %f", t1-t0)
+
+-- watchdog.stop()
 -- local ffi = require('ffi')
 -- ffi.cdef[[
 --     unsigned int sleep(unsigned int seconds);
@@ -16,4 +19,4 @@ watchdog.stop()
 -- log.warn("regular sleep ...")
 -- ffi.C.sleep(2)
 
-os.exit(0)
+-- os.exit(0)
