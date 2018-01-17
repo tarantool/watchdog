a01b1f82449b12f8f4a541b87e18c701042a9637
Yaroslav Dynnikov <yaroslav.dynnikov@gmail.com>
2018-01-17 20:35:22 +0300

    Separate travis makefile

---
+++ .travis.mk
@@ -0,0 +1,12 @@
+all:
+	docker run \
+		--rm=true \
+		--tty=true \
+		-v $(CURDIR):/tmp/watchdog \
+	tarantool/tarantool:1.7 \
+	/bin/sh -c '\
+		apk add --no-cache --virtual .build-deps gcc make cmake musl-dev; \
+		cd /tmp/watchdog; \
+		tarantoolctl rocks make ./rockspecs/watchdog-scm-1.rockspec; \
+		make -C build.luarocks ARGS=-V test; \
+	'
